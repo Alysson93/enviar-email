0a157e531d764dfc209e78c71b1cc8f142d752a8
Alysson Pereira | Grupo Moura <alysson.pereira@grupomoura.com>
2025-02-21 15:50:49 -0300

    read excel and send mails with python

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from settings import Settings
+
+clients = pd.read_excel('clients.xlsx')
+
+for index, client in clients.iterrows():
+    msg = MIMEMultipart()
+    msg['Subject'] = 'E-mail de teste'
+    msg['From'] = Settings().EMAIL
+    msg['To'] = client['Email']
+    message = f'Olá, {client['Nome']}!, você recebeu um e-mail.'
+    msg.attach(MIMEText(message, 'plain'))
+    
+    server = smtplib.SMTP('smtp.gmail.com', port=587)
+    server.starttls()
+    server.login(Settings().EMAIL, Settings().PASSWORD)
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.quit()
